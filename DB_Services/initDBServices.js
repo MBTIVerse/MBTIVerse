769c0769a5d648dcdcdbdba9be89f0e82f93ab7d
뚱댕 <minho@ttungdaeng-ui-MacBookPro.local>
2021-03-10 22:30:11 +0900

    DB_Services init

---
+++ DB_Services/initDBServices.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "MBTIUser",
+        pwd: "12345",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Services"
+            }
+        ]
+    }
+);
